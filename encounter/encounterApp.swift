05843293d0af2667c013892af4186809ddee33cb
Florent Alexandre <florent.alexandre@gmail.com>
2021-05-10 09:50:40 +0200

    Initial Commit

---
+++ encounter/encounterApp.swift
@@ -0,0 +1,20 @@
+//
+//  encounterApp.swift
+//  encounter
+//
+//  Created by Florent Alexandre on 10/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct encounterApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
